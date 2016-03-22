e44f7fb5ef25232c5984018360662bfa1dafad4a
Larissa Amancio <larissa.amancio@outlook.com>
2016-03-22 01:35:24 -0300

    varias coisas

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,20 @@
-class TasksController < ActionController::Base
-		before_action :authenticate_user!
+# encoding: utf-8
+
+class TasksController < ApplicationController
+	before_action :authenticate_user!
+
+	def create
+		@task = Task.new(task_params)
+		@task.save
+	end
+
+	private
+
+	  def task_params
+	    params.require(:task).permit(:text, :list_id)
+	    # params.permit(:name, :privacy)
+
+	  end
+
 
 end
